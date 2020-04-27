52946a7608e21bc9b67826e098ae28438e99b053
Carls13 <charlesshb98@gmail.com>
2020-04-26 23:11:03 -0400

    Updates

---
+++ precache-manifest.7943794a6dbeaa815d61e1c3a12392e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e0dda3d126948408a6602237876648",
+    "url": "/-tracker/index.html"
+  },
+  {
+    "revision": "15a0f84053778f9681dc",
+    "url": "/-tracker/static/js/2.2040b501.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/-tracker/static/js/2.2040b501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bc386e2b204f3fe30a",
+    "url": "/-tracker/static/js/main.7a15ee1b.chunk.js"
+  },
+  {
+    "revision": "309229311c8a25974189",
+    "url": "/-tracker/static/js/runtime-main.5dfb1914.js"
+  },
+  {
+    "revision": "77f4a9b334aa24a6a5f34bfd60be9d09",
+    "url": "/-tracker/static/media/logo.77f4a9b3.png"
+  }
+]);
